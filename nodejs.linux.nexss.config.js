6afd6c03ae98514fecbf04e1dc29a29399028bae
Marcin Polak <mapoart@gmail.com>
2020-09-10 10:43:42 +0200

    Another changes to the linux distributions

--- nodejs.linux.nexss.config.js
+++ nodejs.linux.nexss.config.js
@@ -9,4 +9,18 @@ languageConfig.compilers = {
   },
 };
 
+if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
+  const {
+    replaceCommandByDist,
+    dist,
+  } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
+  // This function just replace all apt-get,apt to the right distribution pkg installer.
+  languageConfig.compilers.node.install = replaceCommandByDist(
+    languageConfig.compilers.node.install
+  );
+
+  languageConfig.dist = dist();
+}
+
 module.exports = languageConfig;
