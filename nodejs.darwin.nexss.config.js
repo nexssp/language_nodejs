8a3fe10d5d88f9b9b09cbb4aac7694599795905d
Marcin Polak <mapoart@gmail.com>
2022-05-29 09:57:53 +0200

    Added sudo to macos config.

--- nodejs.darwin.nexss.config.js
+++ nodejs.darwin.nexss.config.js
@@ -1,22 +1,21 @@
 let languageConfig = Object.assign({}, require("./nodejs.win32.nexss.config"));
-
+let sudo = process.sudo;
 languageConfig.compilers = {
-  node: {
-    install: "brew install nodejs", // but this is installed already.
-    command: "node",
-    args: "<file>",
-    help: ``,
-  },
-  deno: {
-    install:
-      process.replacePMByDistro(`${sudo}apt-get install unzip`) +
-      `
+    node: {
+        install: "brew install nodejs", // but this is installed already.
+        command: "node",
+        args: "<file>",
+        help: ``,
+    },
+    deno: {
+        install: process.replacePMByDistro(`${sudo}apt-get install unzip`) +
+            `
 curl -fsSL https://deno.land/x/install/install.sh | sh`,
-    command: "deno",
-    args: "run <file>",
-    help: ``,
-    templates: "templates_deno",
-  },
+        command: "deno",
+        args: "run <file>",
+        help: ``,
+        templates: "templates_deno",
+    },
 };
 
 module.exports = languageConfig;
