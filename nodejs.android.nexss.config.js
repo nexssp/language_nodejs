8f2c67fa979c157f44cb9720f7a6c8aef38805ce
Marcin Polak <mapoart@gmail.com>
2023-05-07 12:01:35 +0200

    Support for Android platforms

---
+++ nodejs.android.nexss.config.js
@@ -0,0 +1,33 @@
+let languageConfig = Object.assign({}, require("./nodejs.win32.nexss.config"));
+
+let sudo = process.sudo;
+
+languageConfig.compilers = {
+  node: {
+    install: `${sudo}apt install nodejs`, // but this is installed already.
+    command: "node",
+    args: "<file>",
+    help: ``,
+  },
+  deno: {
+    install:
+      process.replacePMByDistro(`${sudo}apt-get install unzip`) +
+      `
+curl -fsSL https://deno.land/x/install/install.sh | sh`,
+    command: "deno",
+    args: "run <file>",
+    help: ``,
+    templates: "templates_deno",
+  },
+};
+
+const distro = process.distro;
+
+// This function just replace all apt-get,apt to the right distribution pkg installer.
+languageConfig.compilers.node.install = process.replacePMByDistro(
+  languageConfig.compilers.node.install
+);
+
+languageConfig.dist = distro;
+
+module.exports = languageConfig;
